290b976a0396c7bbcea449fe7e35421e988d3c4f
unknown <zeldiuss@gmail.com>
2023-03-06 20:58:06 -0300

    upload about

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -8,18 +8,7 @@ function About() {
       <h1 className="btn-shadow">Sobre Mi</h1>
       <div class="cardAbout">
         <div class="card-body">
-          <p class="card-text">¡Hola! Soy Leandro Caballero, un personal trainer apasionado por ayudar a las personas a alcanzar sus metas de fitness y bienestar. Me encanta trabajar en colaboración con mis clientes para desarrollar planes de entrenamiento personalizados y adaptados a sus necesidades y objetivos individuales.
-
-            En el año 2020, logré obtener mi titularidad como personal trainer después de muchos años de dedicación al fitness y al entrenamiento físico. Me especialicé en diferentes áreas, incluyendo entrenamiento de fuerza, resistencia y flexibilidad, así como en nutrición deportiva. Además, estoy siempre aprendiendo y actualizándome en las últimas tendencias y técnicas de entrenamiento para poder brindar el mejor servicio posible a mis clientes.
-
-            Como profesional del fitness, ofrezco una variedad de servicios para ayudar a mis clientes a alcanzar sus objetivos de salud y bienestar. Algunos de mis servicios incluyen:
-
-            Evaluaciones físicas y nutricionales para entender las necesidades y limitaciones de mis clientes.
-            Diseño de planes de entrenamiento personalizados y adaptados a las metas y objetivos individuales.
-            Entrenamiento de fuerza, resistencia y flexibilidad para mejorar la condición física y prevenir lesiones.
-            Asesoramiento nutricional para ayudar a mis clientes a llevar una alimentación saludable y adecuada a sus necesidades.
-            Motivación y apoyo constante para ayudar a mis clientes a mantenerse enfocados y comprometidos con sus objetivos.
-            Si estás buscando un personal trainer que te ayude a alcanzar tus metas de fitness y bienestar, no dudes en contactarme. ¡Estoy aquí para ayudarte!.</p>
+          <p class="card-text">¡Hola! Soy Leandro Caballero, un personal trainer apasionado por ayudar a las personas a alcanzar sus metas de fitness y bienestar. Me especialicé en diferentes áreas, incluyendo entrenamiento de fuerza, resistencia y flexibilidad, así como en nutrición deportiva, y estoy siempre aprendiendo y actualizándome en las últimas tendencias y técnicas de entrenamiento para brindar el mejor servicio posible a mis clientes. Como profesional del fitness, ofrezco una variedad de servicios, incluyendo evaluaciones físicas y nutricionales, diseño de planes de entrenamiento personalizados, asesoramiento nutricional y motivación constante. Si estás buscando un personal trainer que te ayude a alcanzar tus metas de fitness y bienestar, ¡no dudes en contactarme! Estoy aquí para ayudarte.</p>
         </div>
       </div>
 
