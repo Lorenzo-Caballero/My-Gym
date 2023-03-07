5faedf2bc326c284b7c239adf382f472797dedba
unknown <zeldiuss@gmail.com>
2023-03-06 23:05:21 -0300

    added sass contact componen

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,48 +1,64 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './contact.scss';
+import React, { useState } from "react";
+import "./contact.scss";
 
-function Contact() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
+function ContactForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post('/send-email', { name, email, subject, message })
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+    console.log("Form submitted");
+  };
 
   return (
-    <div>
-      <h1>Contacto</h1>
+    <div className="contact-form">
+      <h2 className="form-title">Contactame</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Nombre:</label>
-          <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+        <div className="form-group">
+          <label htmlFor="name" className="form-label">
+            Nombre
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-input"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-        <div>
-          <label htmlFor="email">Correo electrónico:</label>
-          <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <div>
-          <label htmlFor="subject">Asunto:</label>
-          <input type="text" id="subject" name="subject" value={subject} onChange={(e) => setSubject(e.target.value)} />
+        <div className="form-group">
+          <label htmlFor="message" className="form-label">
+            Mensaje
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="form-textarea"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
         </div>
-        <div>
-          <label htmlFor="message">Mensaje:</label>
-          <textarea id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)} />
-        </div>
-        <button type="submit">Enviar</button>
+        <button type="submit" className="form-submit">
+          Enviar
+        </button>
       </form>
     </div>
   );
 }
 
-export default Contact;
+export default ContactForm;
